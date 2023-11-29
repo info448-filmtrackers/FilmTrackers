d8c3adf31e15af4582b703714dd886e10bd58da7
lwong121 <lwong121@uw.edu>
2023-11-29 15:33:12 -0800

    navbar and movie view

--- app/src/main/java/edu/uw/ischool/mwoode/filmtrackers/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/mwoode/filmtrackers/MainActivity.kt
@@ -2,17 +2,42 @@ package edu.uw.ischool.mwoode.filmtrackers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import edu.uw.ischool.mwoode.filmtrackers.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+//import edu.uw.ischool.mwoode.filmtrackers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+//    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(R.layout.activity_main)
-        setContentView(binding.root)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+        val homePageFragment = HomePageFragment();
+        val searchFragment = SearchFragment();
+        val addMovieFragment = AddMovieFragment();
+
+        setFragment(homePageFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> setFragment(homePageFragment)
+                R.id.search -> setFragment(searchFragment)
+                R.id.addMovie -> setFragment(addMovieFragment)
+            }
+            true
+        }
+//        setContentView(binding.root)
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame_layout, fragment);
+            commit();
+        }
     }
 }

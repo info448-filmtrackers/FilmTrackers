c6c8741c6505c36bc3a0c99898b9dc0bddc41ef3
lwong121 <lwong121@uw.edu>
2023-11-29 16:41:54 -0800

    added base url for images

--- app/src/main/java/edu/uw/ischool/mwoode/filmtrackers/Constants.kt
+++ app/src/main/java/edu/uw/ischool/mwoode/filmtrackers/Constants.kt
@@ -1,3 +1,4 @@
 package edu.uw.ischool.mwoode.filmtrackers
 
+const val IMG_BASE_URL = "https://image.tmdb.org/t/p/w500"
 const val BEARER_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2JmOTRjY2RmNzM2Y2JhNmI4NjM2MGI5NTYwZjQ0MiIsInN1YiI6IjY1NjY3ZDkyNmMwYjM2MDBjNzQ1OTVmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3OXy8lCU6T9Fl6AIvrSEPgGVtetETGqYItUuXYKcUJE"

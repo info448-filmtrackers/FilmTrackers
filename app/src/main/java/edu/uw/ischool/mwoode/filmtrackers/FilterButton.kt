d37a3c41620949a989b1cd41549b2739932de4fd
marinawooden <mwoode@uw.edu>
2023-12-06 15:28:19 -0800

    half-search

---
+++ app/src/main/java/edu/uw/ischool/mwoode/filmtrackers/FilterButton.kt
@@ -0,0 +1,69 @@
+package edu.uw.ischool.mwoode.filmtrackers
+
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val TITLE = "title"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FilterButton.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FilterButton : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var title: String? = null;
+    private lateinit var button: Button;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            title = it.getString(TITLE)
+        }
+    }
+
+    fun onClick(callback: () -> Unit) {
+        button.setOnClickListener{
+            callback()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_filter_button, container, false)
+        button = root.findViewById<Button>(R.id.filterButton)
+        button.text = title
+
+        return root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FilterButton.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(title: String) =
+            FilterButton().apply {
+                arguments = Bundle().apply {
+                    putString(TITLE, title)
+                }
+            }
+    }
+}
--- app/src/main/java/edu/uw/ischool/mwoode/filmtrackers/SearchFragment.kt
+++ app/src/main/java/edu/uw/ischool/mwoode/filmtrackers/SearchFragment.kt
@@ -1,19 +1,19 @@
 package edu.uw.ischool.mwoode.filmtrackers
 
+import android.R.attr.button
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.flexbox.FlexboxLayout
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONArray
@@ -22,6 +22,7 @@ import java.util.Timer
 import java.util.TimerTask
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,9 +38,15 @@ class SearchFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var tasksShown: Boolean = false
     private var searchInProg: TimerTask? = null
+    private var filters: Array<String> = arrayOf("Popular", "Niche", "Highly Rated", "Foreign")
+    private val selectedFilters: ArrayList<String> = arrayListOf()
+    private var lastSearch: JSONArray? = null
+    private var filterButtons: ArrayList<FilterButton> = arrayListOf()
+    private var genreFilters: HashMap<Int, String> = hashMapOf<Int, String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // double make sure
 //        arguments?.let {
 //            param1 = it.getString(ARG_PARAM1)
 //            param2 = it.getString(ARG_PARAM2)
@@ -61,16 +68,20 @@ class SearchFragment : Fragment() {
 
             val response = client.newCall(request).execute()
             val check = JSONObject(response.body()?.string())["results"]
+            lastSearch = JSONArray(check.toString())
             displaySearchResults(JSONArray(check.toString()))
         }
     }
 
     fun displaySearchResults(searchResults: JSONArray) {
         Log.i("SEARCH", searchResults.toString())
+        Log.i("FILTER", "SEARCH RESULTS UPDATED")
 
         activity?.runOnUiThread {
             view?.findViewById<LinearLayout>(R.id.searchResultsHolder)?.removeAllViews()
+//            view?.findViewById<FlexboxLayout>(R.id.filtersHolder)?.removeAllViews()
         }
+
 
         for (i in 0 until searchResults.length()) {
             val movieData = searchResults.getJSONObject(i)
@@ -91,6 +102,8 @@ class SearchFragment : Fragment() {
             transaction.commit()
         }
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -122,6 +135,19 @@ class SearchFragment : Fragment() {
             }
         })
 
+        for (i in filters.indices) {
+            Log.i("FILTER", "ADDING " + filters[i])
+            val filterBtn = FilterButton.newInstance(filters[i])
+            // when clicked, it should change background colors, and add itself to the selectedFilters list
+
+            val fragmentManager = childFragmentManager
+            val transaction = fragmentManager.beginTransaction()
+            transaction.add(R.id.filtersHolder, filterBtn)
+            filterButtons.add(filterBtn)
+            // Commit the transaction
+            transaction.commit()
+        }
+
         // open filters functionality
         val filterButton = root.findViewById<RelativeLayout>(R.id.filterHolder)
         filterButton.setOnClickListener {
@@ -131,14 +157,69 @@ class SearchFragment : Fragment() {
         return root;
     }
 
-    private fun toggleFilters() {
+    override fun onStart() {
+        super.onStart()
+        Log.i("Filter", "Starting")
 
+        for (i in filterButtons.indices) {
+            val btn = filterButtons[i]
+            val addToList = fun(): Unit {
+                // check if in selectedFilters
+                if (selectedFilters.contains(filters[i])) {
+                    // if is -> remove
+                    selectedFilters.remove(filters[i])
+                } else {
+                    // if not -> add
+                    selectedFilters.add(filters[i])
+                }
+
+                Log.i("FILTER", filters[i])
+                // display search result
+                val filteredSearchResults = arrayListOf<JSONObject>();
+                if (lastSearch != null) {
+                    for (j in 0 until lastSearch!!.length()) {
+                        val currSearchResult = lastSearch!!.getJSONObject(i)
+                        var shouldBeIncluded = true
+
+                        Log.i("FILTER", currSearchResult.toString())
+                        Log.i("FILTER", selectedFilters.toString())
+                        for (k in 0 until selectedFilters.size) {
+                            when (selectedFilters[k]) {
+                                "Popular" -> shouldBeIncluded = (currSearchResult.getDouble("popularity")) > 100
+                                "Niche" -> shouldBeIncluded = (currSearchResult.getDouble("popularity")) < 150
+                                "Highly Rated" -> shouldBeIncluded = (currSearchResult.getDouble("vote_average") >= 5)
+                                "Foreign" -> shouldBeIncluded = (currSearchResult.getString("original_language") != "en")
+                            }
+
+                            Log.i("FILTER", "EXAMINE FILTER: " + selectedFilters[k])
+
+                            if (!shouldBeIncluded) {
+                                break;
+                            }
+                        }
+
+                        if (shouldBeIncluded) {
+                            filteredSearchResults.add(currSearchResult)
+                        }
+                    }
+                }
+
+
+                displaySearchResults(JSONArray(filteredSearchResults))
+            }
+            btn.onClick(addToList)
+        }
+    }
+
+    private fun toggleFilters() {
         activity?.runOnUiThread {
-            val buttonText = view?.findViewById<TextView>(R.id.filtersText)
+            val filtersHolder = view?.findViewById<FlexboxLayout>(R.id.filtersHolder)
             if (tasksShown) {
-                buttonText?.setText("Filters ▼")
+                // hide filters
+                filtersHolder?.visibility = View.GONE
             } else {
-                buttonText?.setText("Filters ►")
+                // show filters
+                filtersHolder?.visibility = View.VISIBLE
             }
         }
 
